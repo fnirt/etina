4e19d3110c49bdffd5a4dffad9671924ab931d82
Robert Craig <robert.craig@gmail.com>
2020-10-03 18:10:01 -0500

    Add files via upload

--- etina.ino
+++ etina.ino
@@ -1,3 +1,4 @@
+#include "Adafruit_VL53L0X.h"
 
 #define NOTE_C2  65
 #define NOTE_D2  73
@@ -31,90 +32,132 @@
 #define NOTE_A5  880
 #define NOTE_B5  988
 
-int rk1temp = 115;//114
-int rk2temp = 190;//186
-int rk3temp = 280;//279
-int rk4temp = 460;//455
+int rk1temp = 200;//115;//114
+int rk2temp = 300;//190;//186
+int rk3temp = 500;//280;//279
+int rk4temp = 800;//460;//455
 int rk5temp = 1025;//1023
 
-int rk1ptone = NOTE_C4;  char[] rk1pnote = 'C4';
-int rk1dtone = NOTE_B3;  char[] rk1dnote = 'B3';  
-int rk2ptone = NOTE_E4;  char[] rk2pnote = 'E4';
-int rk2dtone = NOTE_D4;  char[] rk2dnote = 'D4';
-int rk3ptone = NOTE_G4;  char[] rk3pnote = 'G4';
-int rk3dtone = NOTE_F4;  char[] rk3dnote = 'F4';
-int rk4ptone = NOTE_C5;  char[] rk4pnote = 'C5';
-int rk4dtone = NOTE_A4;  char[] rk4dnote = 'A4';
-int rk5ptone = NOTE_E5;  char[] rk5pnote = 'E5';
-int rk5dtone = NOTE_B4;  char[] rk5dnote = 'B4';
+int rk1ptone = NOTE_C4;  
+int rk1dtone = NOTE_B3;  
+int rk2ptone = NOTE_E4;  
+int rk2dtone = NOTE_D4;  
+int rk3ptone = NOTE_G4;  
+int rk3dtone = NOTE_F4; 
+int rk4ptone = NOTE_C5;  
+int rk4dtone = NOTE_A4; 
+int rk5ptone = NOTE_E5;  
+int rk5dtone = NOTE_B4;  
 
-int lk1temp = 115;//114
-int lk2temp = 190;//186
-int lk3temp = 280;//279
-int lk4temp = 460;//455
+int lk1temp = 200;//115;//114
+int lk2temp = 300;//190;//186
+int lk3temp = 500;//280;//279
+int lk4temp = 800;//460;//455
 int lk5temp = 1025;//1023
 
-int lk1ptone = NOTE_C2;  char[] lk1pnote = "C2";
-int lk1dtone = NOTE_G2;  char[] lk1dnote = "G2";
-int lk2ptone = NOTE_G2;  char[] lk2pnote = "G2";
-int lk2dtone = NOTE_B2;  char[] lk2dnote = "B2";
-int lk3ptone = NOTE_C3;  char[] lk3pnote = "C3";
-int lk3dtone = NOTE_D3;  char[] lk3dnote = "D3";
-int lk4ptone = NOTE_E3;  char[] lk4pnote = "E3";
-int lk4dtone = NOTE_F3;  char[] lk4dnote = "F3";
-int lk5ptone = NOTE_G3;  char[] lk5pnote = "G3";
-int lk5dtone = NOTE_A3;  char[] lk5dnote = "A3";
+int lk1ptone = NOTE_C2;  
+int lk1dtone = NOTE_G2;  
+int lk2ptone = NOTE_G2;  
+int lk2dtone = NOTE_B2;  
+int lk3ptone = NOTE_C3;  
+int lk3dtone = NOTE_D3;  
+int lk4ptone = NOTE_E3;  
+int lk4dtone = NOTE_F3;  
+int lk5ptone = NOTE_G3;  
+int lk5dtone = NOTE_A3;  
 
 /* 
 
-p	d
-L1	C2	G2
-L2	G2	B2
-L3	C3	D3
-L4	E3	F3
-L5	G3	A3
+p  d
+L1  C2  G2
+L2  G2  B2
+L3  C3  D3
+L4  E3  F3
+L5  G3  A3
 
-L6	B2	A2
-L7	D3	FS3
-L8	G3	A3
-L9	B3	C4
-L10	D4	E4
-		
-R1	C4	B3
-R2	E4	D4
-R3	G4	F4
-R4	C5	A4
-R5	E5	B4
+L6  B2  A2
+L7  D3  FS3
+L8  G3  A3
+L9  B3  C4
+L10 D4  E4
+    
+R1  C4  B3
+R2  E4  D4
+R3  G4  F4
+R4  C5  A4
+R5  E5  B4
 
-R6	G4	FS4
-R7	B4	A4
-R8	D5	C5
-R9	G5	E5
-R10	B5	FS5
+R6  G4  FS4
+R7  B4  A4
+R8  D5  C5
+R9  G5  E5
+R10 B5  FS5
 
 */
 int iLED = 13;
 int iButtonPinL = A1; // keys R1-5
-int iButtonPinR = A0; // keys L1-5
+int iButtonPinR = A2; // keys L1-5
 int iPushButton = 2;
 int Piezo = 12;
 
+Adafruit_VL53L0X lox = Adafruit_VL53L0X();
+int lastreading = 0;
 
 boolean bPushing = false;
 
 void setup()
 {
   Serial.begin(9600);
+  // wait until serial port opens for native USB devices
+  while (! Serial) {
+    //delay(1);
+  }
   pinMode(iPushButton, INPUT);
   pinMode(iLED, OUTPUT); 
   pinMode(Piezo, OUTPUT);
   noTone(Piezo);
+
+    Serial.println("Adafruit VL53L0X test");
+  if (!lox.begin()) {
+    Serial.println(F("Failed to boot VL53L0X"));
+    while(1);
+  }
+
 }
 
 void loop()
 {
 
+ VL53L0X_RangingMeasurementData_t measure;
+
+  //Serial.print("Reading a measurement... ");
+  lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
+
+  if (measure.RangeStatus != 4) {  // phase failures have incorrect data
+    Serial.print("D: "); Serial.print(measure.RangeMilliMeter);
+    int thisreading = measure.RangeMilliMeter;
+    if (thisreading < 1) {
+       Serial.print("      _      ");
+      
+    } else {
+      if (thisreading  < lastreading) {
+        Serial.print("        push ");
+        bPushing = true;
+
+      } else {
+        Serial.print("  pull       ");
+        bPushing = false;
+        bPushing = true;
+      }
+      
+    }
+    lastreading = thisreading;
+  } else {
+       Serial.print("             ");
+  }
+    
   // directional (push) button
+/*  
   int buttonState = digitalRead(iPushButton);
   if (buttonState == 1) {
     bPushing = true;
@@ -125,20 +168,21 @@ void loop()
    }
   Serial.print((String)" pushing: " + bPushing);
 
+ */
   // right hand keys
   int temp = analogRead(iButtonPinR);   //Read the analogue input
   //Serial.println("temp");               //Display the read value in the Serial monitor
-  Serial.print((String)" Rtemp: "+temp);               //Display the read value in the Serial monitor
-  if (temp == 0) 
+  Serial.print((String)" R: "+temp);               //Display the read value in the Serial monitor
+  if (temp < 10 ) 
   {
-    noTone(Piezo);
+    //noTone(Piezo);
   }
   else if (temp < rk1temp)                     //Lower limit for first button - if below this limit then no button is pushed and LEDs are turned off
   {
     if (bPushing == 1)  
-    { playsound(rk1ptone);}
+    { playsound(rk1ptone);   } 
     else 
-    { playsound(rk1dtone);}
+    { playsound(rk1dtone);   }
   }
   else if (temp < rk2temp)                     //Lower limit for first button - if below this limit then no button is pushed and LEDs are turned off
   {
@@ -167,16 +211,19 @@ void loop()
     { playsound(rk5ptone);   } 
     else 
     { playsound(rk5dtone);   }
+  } else {
+      //noTone(Piezo);
+
   }
 
 
   // left hand keys
   temp = analogRead(iButtonPinL);   //Read the analogue input
   //Serial.println("temp");               //Display the read value in the Serial monitor
-  Serial.print((String)" Ltemp: " + temp);               //Display the read value in the Serial monitor
-  if (temp == 0) 
+  Serial.print((String)" L: " + temp);               //Display the read value in the Serial monitor
+  if (temp <10000 ) 
   {
-    noTone(Piezo);
+    //noTone(Piezo);
   }
   else if (temp < lk1temp)                     //Lower limit for first button - if below this limit then no button is pushed and LEDs are turned off
   {
@@ -212,22 +259,21 @@ void loop()
     { playsound(lk5ptone);   } 
     else 
     { playsound(lk5dtone);   }
+  } else {
+      //noTone(Piezo);
+
   }
 
   //clean up
   bPushing = false;
-  delay(10); // Delay a little bit to improve simulation performance
-  noTone(Piezo);
+  //delay(10); // Delay a little bit to improve simulation performance
+ // noTone(Piezo);
   Serial.println("!");               //Display the read value in the Serial monitor
 
 }
 
 void playsound(double note) {
-    Serial.print((String)" note "+note);               //Display the read value in the Serial monitor
-    tone(Piezo, note, 500);	// plays note
-}
-
-void shownote(String note) {
-    Serial.print((String)" note "+note);
+    Serial.print((String)" N: "+note);               //Display the read value in the Serial monitor
+    tone(Piezo, note, 500); // plays note
 }
  
